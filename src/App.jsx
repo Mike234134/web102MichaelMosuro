542d31b8a2c2a0e3f51e272a041c9665a5200b57
Michael Mosuro <mimos3@morgan.edu>
2025-03-02 12:38:43 -0500

    this is project2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,72 @@
 import './App.css';
-import Calendar from './components/Calendar'
+import {useState} from "react";
 
 const App = () => {
+  const[count,Setcount]=useState(0)
+  const [multiplier,Setmultiplier]=useState(1)
+  const UpdateClick = () => Setcount(count+multiplier)
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      Setcount(count-10)
+      Setmultiplier(multiplier * 2);
+    }else if(count<10){
+      alert("you dont have enough points for this purchase")
 
-  return (
+    }
+  }
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      Setcount(count-100)
+      Setmultiplier(multiplier * 5);
+    }else if(count<100){
+      alert("you dont have enough points for this purchase")
+
+    }
+  }
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      Setcount(count-1000)
+      Setmultiplier(multiplier * 10);
+    }else if(count<1000){
+      alert("you dont have enough points for this purchase")
+
+    }
+  }
+
+  
+  return ( 
+    <>
     <div className="App">
-    <h1>Itinerary for 7 Days in Chicago</h1>
-    <h2>Welcome to Chicago,Natalia! Check out this calendar to get know the city and see the sights during your stay. </h2>
-    <Calendar/>
-    </div>
+      <div className="Header">
+        <h1>Samosa Clicker</h1>
+        <h2>Count {count}</h2>
+        
+        <img className="Samosa" src="food_16267410.png" onClick={UpdateClick}/>
+          <div className="Container">
+            <div className="upgrade">
+              <h3>Double Stuffed 👯‍♀️</h3>
+              <p>2x per click</p>
+              <button onClick={buyDoubleStuffed}> 10 Samosas</button>
+            </div>
+            <div className="upgrade">
+              <h3>Party Pack 🎉</h3>
+              <p>5x per click</p>
+              <button onClick={buyPartyPack}> 100 Samosas</button>
+            </div>
+            <div className="upgrade">
+              <h3>Full Feast 👩🏽‍🍳</h3>
+              <p>10x per click</p>
+              <button onClick={buyFullFeast}> 1k Samosas</button>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+      
+    
+      
+    
+    </>
   )
 }
-
 export default App
